895fa6e1dc80b96af139fb32c8e19677cbeb482a
alevlon <ask.alevlon@gmail.com>
2022-04-01 14:26:24 +0300

    Prac #2 completed

---
+++ prac-2/prac-2/GeneticWindow.xaml.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using static System.Math;
+
+namespace prac_2
+{
+    /// <summary>
+    /// Логика взаимодействия для GeneticWindow.xaml
+    /// </summary>
+    public partial class GeneticWindow : Window
+    {
+        static DispatcherTimer dT;
+        static int Radius = 15;
+        static int PointCount = 5;
+        static Polygon myPolygon = new Polygon();
+        static List<Ellipse> EllipseArray = new List<Ellipse>();
+        static PointCollection pC = new PointCollection();
+        static double percentMut = 0.9;
+        List<List<int>> Population = new List<List<int>>();
+        List<double> Roads = new List<double>();
+        static int sizePopulation = PointCount;
+
+        public GeneticWindow()
+        {
+            dT = new DispatcherTimer();
+
+            CreateRandomPopulation(Population);
+           
+
+            InitializeComponent();
+            InitPoints();
+            InitPolygon();
+
+            CalculateRoads(Population);
+            dT = new DispatcherTimer();
+            dT.Tick += new EventHandler(OneStep);
+            dT.Interval = new TimeSpan(0, 0, 0, 0, 1000);
+        }
+        private void InitPoints()
+        {
+            Random rnd = new Random();
+            pC.Clear();
+            EllipseArray.Clear();
+            myPolygon = new Polygon();
+
+            for (int i = 0; i < PointCount; i++)
+            {
+                Point p = new Point();
+
+                p.X = rnd.Next(Radius, (int)(0.75 * GeneticAlgWindow.Width) - 3 * Radius);
+                p.Y = rnd.Next(Radius, (int)(0.90 * GeneticAlgWindow.Height - 3 * Radius));
+                pC.Add(p);
+            }
+
+            for (int i = 0; i < PointCount; i++)
+            {
+                Ellipse el = new Ellipse();
+
+                el.StrokeThickness = 3;
+                el.Height = el.Width = Radius;
+                el.Stroke = Brushes.Red;
+                el.Fill = Brushes.Yellow;
+                EllipseArray.Add(el);
+            }
+        }
+
+        private void InitPolygon()
+        {
+            myPolygon.Stroke = System.Windows.Media.Brushes.WhiteSmoke;
+            myPolygon.StrokeThickness = 2;
+        }
+
+        private void PlotPoints()
+        {
+            for (int i = 0; i < PointCount; i++)
+            {
+                Canvas.SetLeft(EllipseArray[i], pC[i].X - Radius / 2);
+                Canvas.SetTop(EllipseArray[i], pC[i].Y - Radius / 2);
+                MyCanvas.Children.Add(EllipseArray[i]);
+            }
+        }
+
+
+        private void PlotWay(List<int> BestWayIndex)
+        {
+            PointCollection Points = new PointCollection();
+
+            for (int i = 0; i < BestWayIndex.Count; i++)
+                Points.Add(pC[BestWayIndex[i]]);
+
+            myPolygon.Points = Points;
+            MyCanvas.Children.Add(myPolygon);
+        }
+
+        private void VelCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox CB = (ComboBox)e.Source;
+            ListBoxItem item = (ListBoxItem)CB.SelectedItem;
+
+            dT.Interval = new TimeSpan(0, 0, 0, 0, Convert.ToInt16(item.Content));
+        }
+
+        private void StopStart_Click(object sender, RoutedEventArgs e)
+        {
+            if (dT.IsEnabled)
+            {
+                dT.Stop();
+                NumElemCB.IsEnabled = true;
+            }
+            else
+            {
+                NumElemCB.IsEnabled = false;
+                dT.Start();
+            }
+        }
+
+        private void NumElemCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox CB = (ComboBox)e.Source;
+            ListBoxItem item = (ListBoxItem)CB.SelectedItem;
+
+            PointCount = Convert.ToInt32(item.Content);
+            CreateRandomPopulation(Population);
+            
+
+            InitPoints();
+            InitPolygon();
+
+            CalculateRoads(Population);
+        }
+
+        private void OneStep(object sender, EventArgs e)
+        {
+            MyCanvas.Children.Clear();
+            //InitPoints();
+            PlotPoints();
+            PlotWay(GetBestWay());
+        }
+
+        private List<int> GetBestWay()
+        {
+            Random rnd = new Random();
+            List<int> way = new List<int>();
+
+            int x = rnd.Next(0, sizePopulation);
+            int y = rnd.Next(0, sizePopulation);
+            while (x == y) y = rnd.Next(0, sizePopulation);
+
+            List<int> firstChild = new List<int>();
+            List<int> secondChild = new List<int>();
+
+
+            //скрещивание
+            int gap = rnd.Next(2, PointCount - 1); 
+            for (int i = 0; i < gap; i++) 
+            {
+                firstChild.Add(Population[x][i]);
+                secondChild.Add(Population[y][i]);
+            }
+            for (int i = gap; i < PointCount; i++) 
+            {
+                int k1 = Population[y][i];
+                if (!firstChild.Contains(k1)) firstChild.Add(k1);
+
+                int k2 = Population[x][i];
+                if (!secondChild.Contains(k2)) secondChild.Add(k2);
+            }
+            for (int i = gap; i < PointCount; i++)
+            {
+                int k1 = Population[x][i];
+                if (!firstChild.Contains(k1)) firstChild.Add(k1);
+
+                int k2 = Population[y][i];
+                if (!secondChild.Contains(k2)) secondChild.Add(k2);
+            }
+
+            //мутации
+            Mutation(firstChild);
+            Mutation(secondChild);
+
+            double firstChildRoad = CalculateRoad(firstChild);
+            double secondChildRoad = CalculateRoad(secondChild);
+
+            Population.Add(firstChild);
+            Roads.Add(firstChildRoad);
+            Population.Add(secondChild);
+            Roads.Add(secondChildRoad);
+
+            var d = Roads.Count / 2;
+            while (d >= 1)
+            {
+                for (var i = d; i < Roads.Count; i++)
+                {
+                    var j = i;
+                    while ((j >= d) && (Roads[j - d] > Roads[j]))
+                    {
+                        double t = Roads[j];
+                        Roads[j] = Roads[j - d];
+                        Roads[j - d] = t;
+
+                        List<int> f = Population[j];
+                        Population[j] = Population[j - d];
+                        Population[j - d] = f;
+
+                        j = j - d;
+                    }
+                }
+
+                d = d / 2;
+            }
+
+            Roads.RemoveAt(Roads.Count - 2);
+            Roads.RemoveAt(Roads.Count - 1);
+            Population.RemoveAt(Population.Count - 2);
+            Population.RemoveAt(Population.Count - 1);
+
+
+            way = Population[0];
+            return way;
+        }
+
+        static void Mutation(List<int> arr) 
+        {
+            Random rnd = new Random();
+            int kef = rnd.Next(0, 100);
+            if (percentMut * 100 >= kef) 
+            {
+                int k1 = rnd.Next(0, PointCount);
+                int k2 = rnd.Next(0, PointCount);
+                while (k1 == k2) k2 = rnd.Next(0, PointCount);
+
+                int t = arr[k1];
+                arr[k1] = arr[k2];
+                arr[k2] = t;
+            }
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            dT.Stop();
+
+            MainWindow mwd = new MainWindow();
+            mwd.Show();
+            Close();
+        }
+        private void CreateRandomPopulation(List<List<int>> arr) 
+        {
+            arr.Clear();
+            Random rnd = new Random();
+            for (int i = 0; i < sizePopulation; i++) 
+            {
+                arr.Add(new List<int>());
+                arr[i] = new List<int>();
+                for (int j = 0; j < PointCount; j++) 
+                {
+                    int x = rnd.Next(0, PointCount);
+                    while (CheckForUniq(arr[i], x) == false) x = rnd.Next(0, PointCount);
+                    arr[i].Add(x);
+                }
+            }
+        }
+        private double CalculateRoad(List<int> arr)
+        {
+            double sum = 0;
+            for (int i = 1; i < arr.Count; i++) sum += Sqrt(Pow(pC[arr[i]].X - pC[arr[i - 1]].X, 2) + Pow(pC[arr[i]].Y - pC[arr[i - 1]].Y, 2));
+
+            return sum;
+        }
+        private void CalculateRoads(List<List<int>> arr)
+        {
+            Roads.Clear();
+            for (int i = 0; i < arr.Count; i++) 
+            {
+                double sum = 0;
+                for (int j = 1; j < arr[i].Count; j++) 
+                {
+                    sum += Sqrt(Pow(pC[arr[i][j]].X - pC[arr[i][j - 1]].X, 2) + Pow(pC[arr[i][j]].Y - pC[arr[i][j - 1]].Y, 2));
+                }
+                Roads.Add(sum);
+            }
+
+        }
+        private bool CheckForUniq(List<int> arr, int x) 
+        {
+            bool check = true;
+
+            for (int i = 0; i < arr.Count; i++) 
+            {
+                if (x == arr[i]) 
+                {
+                    check = false;
+                    break;
+                } 
+            }
+            return check;
+        }
+    }
+}
---
+++ prac-2/prac-2/GreedyWindow.xaml.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using static System.Math;
+
+namespace prac_2
+{
+    /// <summary>
+    /// Логика взаимодействия для GreedyWindow.xaml
+    /// </summary>
+    public partial class GreedyWindow : Window
+    {
+        Random rnd = new Random();
+        static DispatcherTimer dT;
+        static int Radius = 20;
+        static int PointCount = 5;
+        static Polygon myPolygon = new Polygon();
+        static List<Ellipse> EllipseArray = new List<Ellipse>();
+        static PointCollection pC = new PointCollection();
+        static List<int> QueuePoint = new List<int>();
+        static List<int> ListForCheck = new List<int>();
+        static int currentPoint;
+        static double minLength = double.MaxValue;
+        static int closePoint = int.MaxValue;
+
+        public GreedyWindow()
+        {
+            dT = new DispatcherTimer();
+            currentPoint = rnd.Next(1, PointCount);
+
+            InitializeComponent();
+            InitPoints();
+            InitPolygon();
+
+            dT = new DispatcherTimer();
+            dT.Tick += new EventHandler(OneStep);
+            dT.Interval = new TimeSpan(0, 0, 0, 0, 1000);
+
+            EnterThePoints(ListForCheck, currentPoint);
+            QueuePoint.Clear();
+            QueuePoint.Add(currentPoint);
+
+        }
+        private void InitPoints()
+        {
+            Random rnd = new Random();
+            pC.Clear();
+            EllipseArray.Clear();
+            myPolygon = new Polygon();
+
+            for (int i = 0; i < PointCount; i++)
+            {
+                Point p = new Point();
+
+                p.X = rnd.Next(Radius, (int)(0.75 * GreedyAlgWindow.Width) - 3 * Radius);
+                p.Y = rnd.Next(Radius, (int)(0.90 * GreedyAlgWindow.Height - 3 * Radius));
+                pC.Add(p);
+            }
+
+            for (int i = 0; i < PointCount; i++)
+            {
+                Ellipse el = new Ellipse();
+
+                el.StrokeThickness = 3;
+                el.Height = el.Width = Radius;
+                el.Stroke = Brushes.Red;
+                el.Fill = Brushes.Yellow;                
+                EllipseArray.Add(el);
+            }
+        }
+
+        private void InitPolygon()
+        {
+            myPolygon.Stroke = System.Windows.Media.Brushes.WhiteSmoke;
+            myPolygon.StrokeThickness = 2;
+        }
+
+        private void PlotPoints()
+        {
+            for (int i = 0; i < PointCount; i++)
+            {
+                Canvas.SetLeft(EllipseArray[i], pC[i].X - Radius / 2.0);
+                Canvas.SetTop(EllipseArray[i], pC[i].Y - Radius / 2.0);
+                MyCanvas.Children.Add(EllipseArray[i]);
+            }
+        }
+
+
+        private void PlotWay(int[] BestWayIndex)
+        {
+            PointCollection Points = new PointCollection();
+
+            for (int i = 0; i < BestWayIndex.Length; i++)
+                Points.Add(pC[BestWayIndex[i]]);
+
+            myPolygon.Points = Points;
+            MyCanvas.Children.Add(myPolygon);
+        }
+
+        private void VelCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox CB = (ComboBox)e.Source;
+            ListBoxItem item = (ListBoxItem)CB.SelectedItem;
+
+            dT.Interval = new TimeSpan(0, 0, 0, 0, Convert.ToInt16(item.Content));
+        }
+
+        private void StopStart_Click(object sender, RoutedEventArgs e)
+        {
+            if (dT.IsEnabled)
+            {
+                dT.Stop();
+                NumElemCB.IsEnabled = true;
+            }
+            else
+            {
+                NumElemCB.IsEnabled = false;
+                dT.Start();
+            }
+        }
+
+        private void NumElemCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox CB = (ComboBox)e.Source;
+            ListBoxItem item = (ListBoxItem)CB.SelectedItem;
+
+            PointCount = Convert.ToInt32(item.Content);
+            currentPoint = rnd.Next(1, PointCount);
+
+            ListForCheck.Clear();
+            EnterThePoints(ListForCheck, currentPoint);
+            QueuePoint.Clear();
+            QueuePoint.Add(currentPoint);
+            minLength = double.MaxValue;
+            closePoint = int.MaxValue;
+
+            InitPoints();
+            InitPolygon();
+        }
+
+        private void OneStep(object sender, EventArgs e)
+        {
+            if (QueuePoint.Count >= PointCount)
+            {
+                MessageBox.Show("Найкоротший шлях знайдено");
+                dT.Stop();
+            }
+            else 
+            {
+                MyCanvas.Children.Clear();
+                //InitPoints();
+                PlotPoints();
+                PlotWay(GetBestWay());
+            }
+        }
+
+        private int[] GetBestWay()
+        {
+            int[] way = new int[QueuePoint.Count + 1];
+            for (int i = 0; i < QueuePoint.Count; i++) way[i] = QueuePoint[i];
+
+            if (ListForCheck.Count == 0)
+            {
+                way[way.Length - 1] = closePoint;
+                QueuePoint.Add(closePoint);
+                minLength = double.MaxValue;
+                closePoint = int.MaxValue;
+                EnterThePoints(ListForCheck, QueuePoint);
+            }
+            else 
+            {
+                way[way.Length - 1] = ListForCheck[0];
+                double road = Sqrt(Pow(pC[way[way.Length - 1]].X - pC[way[way.Length - 2]].X, 2) + Pow(pC[way[way.Length - 1]].Y - pC[way[way.Length - 2]].Y, 2));
+                if (road < minLength)
+                {
+                    closePoint = way[way.Length - 1];
+                    minLength = road;
+                }
+                ListForCheck.RemoveAt(0);
+            }
+            
+
+            return way;
+        }
+
+
+
+
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            dT.Stop();
+            ListForCheck.Clear();
+            QueuePoint.Clear();
+
+            MainWindow mwd = new MainWindow();
+            mwd.Show();
+            Close();
+        }
+        private void EnterThePoints(List<int> arr, int currentPoint) 
+        {
+            arr.Clear();
+            for (int i = 0; i < PointCount; i++) 
+            {
+                if (currentPoint == i) continue;
+
+                arr.Add(i);
+            }
+        }
+        private void EnterThePoints(List<int> arr, List<int> mass)
+        {
+            arr.Clear();
+            for (int i = 0; i < PointCount; i++)
+            {
+                if (mass.Contains(i)) continue;
+
+                arr.Add(i);
+            }
+        }
+    }
+}
